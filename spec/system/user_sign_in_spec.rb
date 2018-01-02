1e77081f4645e13968cf3db933d8df1250f34dc7
yasaichi <yasaichi@users.noreply.github.com>
2018-01-02 18:44:42 +0900

    Write tests of sign in page

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "User sign in", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    visit new_user_session_path
+  end
+
+  it "should allow users to sign in with their email and password" do
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+
+    expect(page).to have_current_path(root_path)
+  end
+end
